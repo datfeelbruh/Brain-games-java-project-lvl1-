01b40929253cc98fd34ed4753b0c4176bfb03223
khoroshko.d <datfeelman@gmail.com>
2022-06-20 13:15:42 +0300

    Add Calc game logic

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,12 +1,14 @@
 package hexlet.code;
 
 import hexlet.code.games.Even;
+import hexlet.code.games.Calc;
 
 public class App {
     public static void main(String[] args) {
         switch (Cli.chooseGame()) {
             case 1 -> Cli.greeting();
             case 2 -> Even.start();
+            case 3 -> Calc.start();
             default -> {
             }
         }
---
+++ src/main/java/hexlet/code/games/Calc.java
@@ -0,0 +1,49 @@
+package hexlet.code.games;
+
+import hexlet.code.Cli;
+
+public class Calc {
+    public static void start() {
+        Cli.greeting();
+        System.out.println("What is the result of the expression?");
+        run();
+    }
+
+    private static void run() {
+        String[] operators = {"+", "-", "*"};
+        final int maxRounds = 3;
+        final int range = 15;
+        int rounds = 0;
+        while (rounds < maxRounds) {
+            int a = (int) (Math.random() * range);
+            int b = (int) (Math.random() * range);
+            int operatorsRange = operators.length - 1;
+            String chosenOperator = operators[(int) (Math.round(Math.random() * operatorsRange))];
+            String expression = Integer.toString(a) + " " + chosenOperator + " " + Integer.toString(b);
+            System.out.println("Question: " + expression);
+            System.out.print("Your answer: ");
+            int answer = Integer.parseInt(Cli.getUserInput().nextLine());
+            if (calcExpression(a, b, chosenOperator) == answer) {
+                System.out.println("Correct");
+                if (rounds == 2) {
+                    System.out.println("Congratulation, " + Cli.getPlayerName());
+                }
+                rounds++;
+            } else {
+                System.out.println("'" + Integer.toString(answer) + "'" + " is wrong answer ;(. "
+                                    + "Correct answer was " + "'" + calcExpression(a, b, chosenOperator) + "'.\n"
+                                    + "Let's try again, " + Cli.getPlayerName());
+                rounds = maxRounds;
+            }
+        }
+    }
+
+    private static int calcExpression(int a, int b, String operator) {
+        return switch (operator) {
+            case "+" -> a + b;
+            case "-" -> a - b;
+            case "*" -> a * b;
+            default -> 0;
+        };
+    }
+}
