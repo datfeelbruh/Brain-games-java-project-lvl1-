8d75f539cfafa4839914d5614a170e6d4b2302bf
khoroshko.d <datfeelman@gmail.com>
2022-06-20 17:18:47 +0300

    Update App

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -4,15 +4,19 @@ import hexlet.code.games.Even;
 import hexlet.code.games.Calc;
 import hexlet.code.games.Gcd;
 import hexlet.code.games.Progression;
+import hexlet.code.games.Prime;
+
+
 
 public class App {
     public static void main(String[] args) {
         switch (Cli.chooseGame()) {
-            case 1 -> Cli.greeting();
-            case 2 -> Even.start();
-            case 3 -> Calc.start();
-            case 4 -> Gcd.start();
-            case 5 -> Progression.start();
+            case "1" -> Cli.greeting();
+            case "2" -> Even.start();
+            case "3" -> Calc.start();
+            case "4" -> Gcd.start();
+            case "5" -> Progression.start();
+            case "6" -> Prime.start();
             default -> {
             }
         }
