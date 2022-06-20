26d97f71af4c952b17531152889ec96f5e293391
khoroshko.d <datfeelman@gmail.com>
2022-06-20 15:15:08 +0300

    Update App

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -3,6 +3,7 @@ package hexlet.code;
 import hexlet.code.games.Even;
 import hexlet.code.games.Calc;
 import hexlet.code.games.Gcd;
+import hexlet.code.games.Progression;
 
 public class App {
     public static void main(String[] args) {
@@ -11,6 +12,7 @@ public class App {
             case 2 -> Even.start();
             case 3 -> Calc.start();
             case 4 -> Gcd.start();
+            case 5 -> Progression.start();
             default -> {
             }
         }
