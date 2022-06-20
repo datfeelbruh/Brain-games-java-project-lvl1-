0dbbcbe5a8523f913a8cb1fd9510466de18fa559
khoroshko.d <datfeelman@gmail.com>
2022-06-20 12:46:07 +0300

    Update logic Cli

--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -2,22 +2,42 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    static String playerName = "";
+    private static String playerName = "";
+    private static Scanner userInput = new Scanner(System.in);
+
     public static void greeting() {
         System.out.print("Welcome to the Brain Games!\nMay I have your name? ");
-        Scanner sc = new Scanner(System.in);
-        playerName = sc.nextLine();
-        System.out.println("Hello, " + playerName + "!");
+        setPlayerName();
+        System.out.println("Hello, " + getPlayerName() + "!");
     }
 
-    public static int choice() {
-        System.out.println("Please enter the game number and press Enter\n" +
-                            "1 - Greet\n" +
-                            "2 - Even\n" +
-                            "0 - Exit");
+    public static String getPlayerName() {
+        return playerName;
+    }
+
+    private static void setPlayerName() {
+        playerName = userInput.nextLine();
+    }
+
+    public static int chooseGame() throws NumberFormatException {
+        int playerChoice = -1;
+        System.out.println("Please enter the game number and press Enter\n"
+                            + "1 - Greet\n"
+                            + "2 - Even\n"
+                            + "0 - Exit");
         System.out.print("Your choice: ");
-        Scanner sc = new Scanner(System.in);
-        int choice = sc.nextInt();
-        return choice;
+        try {
+            playerChoice = Integer.parseInt(userInput.nextLine());
+        } catch (NumberFormatException e) {
+            System.out.println("You must input a number");
+        }
+        return playerChoice;
+    }
+    public static void closeUserInput() {
+        userInput.close();
+    }
+
+    public static Scanner getUserInput() {
+        return userInput;
     }
 }
