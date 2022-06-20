bef196605c9f78a09ded24007b1ae9cd18f5754e
khoroshko.d <datfeelman@gmail.com>
2022-06-20 12:45:33 +0300

    Update logic App

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,12 +1,17 @@
 package hexlet.code;
+
+import hexlet.code.games.Even;
+
 public class App {
     public static void main(String[] args) {
-        switch (Cli.choice()) {
-            case 1:
-                Cli.greeting();
-                break;
-            case 2:
-                Even.start();
+        switch (Cli.chooseGame()) {
+            case 1 -> Cli.greeting();
+            case 2 -> Even.start();
+            default -> {
+            }
         }
+
+        Cli.closeUserInput();
+
     }
 }
