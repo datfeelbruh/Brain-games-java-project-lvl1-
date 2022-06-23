5d29f6981d682ee6625d04044767eb117737c726
khoroshko.d <datfeelman@gmail.com>
2022-06-23 15:18:13 +0300

    Add javadoc for methods

--- src/main/java/hexlet/code/Engine.java
+++ src/main/java/hexlet/code/Engine.java
@@ -5,6 +5,15 @@ import java.util.Scanner;
 public class Engine {
     public static final int MAXROUNDS = 3;
 
+    /**
+     * The method that processes the data of the game that invoke this method. In cycle print <code>gameData[0][i]</code> question
+     * and get player answer. Compare <code>gameData[1][i]</code> answer and player answer.
+     * @author datfeelbruh <datfeelman@gmail.com>
+     * @see hexlet.code.games
+     * @param gameData <code>gameData[0]</code> - question;
+     *                <code>gameData[1]</code> answers;
+     * @param description The game rules <code>description</code> to be printed.
+     */
     public static void run(String[][] gameData, String description) {
         System.out.print("Welcome to the Brain Games!\nMay I have your name? ");
         Scanner scanner = new Scanner(System.in);
--- src/main/java/hexlet/code/Utils.java
+++ src/main/java/hexlet/code/Utils.java
@@ -1,6 +1,13 @@
 package hexlet.code;
 
 public class Utils {
+    /**
+     * The method return random number in range from <code>minValue</code> to <code>maxValue</code>
+     * @author datfeelbruh <datfeelman@gmail.com>
+     * @see hexlet.code.games
+     * @param minValue minimal number which method can generate
+     * @param maxValue maximum number which method can generate
+     */
     public static int randomNumber(int minValue, int maxValue) {
         return minValue + (int) (Math.random() * maxValue);
     }
